8b788dd4a430ff5d18177a1ea3a5e8d53b5ad2a3
Todd Kummer <todd@rockridgesolutions.com>
2025-05-12 14:04:34 -0700

    Add importmap for controllers
    This will add the controllers to the import map of the main app. I'm assuming the "importmap" initializer does not fire if importmap is not in use.

--- lib/bulma/engine.rb
+++ lib/bulma/engine.rb
@@ -2,6 +2,10 @@ require "rails/engine"
 
 module Bulma
   class Engine < ::Rails::Engine
+    initializer "bulma.importmap", before: "importmap" do |app|
+      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+    end
+
     initializer "bulma.assets" do |app|
       app.config.assets.paths << root.join("app/javascript")
     end
